c5196a0577441c514005c4d213e40e00cfeb36b9
Tataihono Nikora <tataihono.nikora@gmail.com>
2026-02-19 11:36:23 +1300

    fix(web): enforce required environment variables

--- apps/web/src/lib/client.ts
+++ apps/web/src/lib/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 import { env } from "@/env"
 
-const uri = env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:1337/graphql"
+const uri = env.NEXT_PUBLIC_GRAPHQL_URL
 const headers: Record<string, string> = {}
 if (env.STRAPI_API_TOKEN) {
   headers.Authorization = `Bearer ${env.STRAPI_API_TOKEN}`
