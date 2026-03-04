73ce0b44d1ede132fc505b20fdb8a1a8aa4040f2
Tataihono Nikora <tataihono.nikora@gmail.com>
2026-03-04 22:33:40 +1300

    fix(infra): grant Terraform apply role Secrets Manager permissions for RDS (#197)

--- infra/aws/github/terraform.tf
+++ infra/aws/github/terraform.tf
@@ -95,6 +95,7 @@ data "aws_iam_policy_document" "github_actions_terraform_apply" {
       "kms:*",
       "logs:*",
       "rds:*",
+      "secretsmanager:*",
       "route53:*",
       "s3:*",
       "wafv2:*"
